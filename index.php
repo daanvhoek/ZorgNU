dccb8f069798dabc8aa466368a9c455120e38171
daanvhoek <95030392+daanvhoek@users.noreply.github.com>
2022-09-29 15:30:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZorgNU</title>
+
+    <!-- swiper css-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
+    <!-- font link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<!-- header section starts-->
+
+<section class="header">
+
+    <a href="index.php" class="logo">logo.</a>
+
+    <nav class="navbar">
+        <a href="index.php">home</a>
+        <a href="over%20mij.php.php">over mij</a>
+        <a href="contact.php">contact</a>
+        <a href="diensten.php">diensten</a>
+    </nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- js link -->
+<script src="js.js"></script>
+
+</body>
+</html>
